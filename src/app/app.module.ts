0cc16c4244edb456b19abf7c41e67040c59effb1
abd-elilah <abdelilaboudribila0712@gmail.com>
2022-06-07 11:47:30 +0100

    structuring the project in a module way

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
+import { LoginModule } from './Authenticat/login.module';
+import { ClientModule } from './client/client.module';
+import { CompteModule } from './compte/compte.module';
+import { HomeModule } from './home/home.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { SharedModule } from './shared/shared.module';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    HomeModule,
+    CompteModule,
+    ClientModule,
+    LoginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
